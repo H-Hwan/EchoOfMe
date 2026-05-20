3e347b27d683c0736b183af75b0f15a1b5b54501
i69541620-glitch <wjdghks2339@gmail.com>
2026-05-20 17:40:16 +0900

    프로젝트 세팅

--- Source/EchoOfMe.Target.cs
+++ Source/EchoOfMe.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class EchoOfMeTarget : TargetRules
 {
-	public EchoOfMeTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
-		ExtraModuleNames.Add("EchoOfMe");
-	}
+    public EchoOfMeTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+
+        ExtraModuleNames.Add("EchoOfMe");
+    }
 }
--- Source/EchoOfMeEditor.Target.cs
+++ Source/EchoOfMeEditor.Target.cs
@@ -1,15 +1,15 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 using UnrealBuildTool;
 using System.Collections.Generic;
 
 public class EchoOfMeEditorTarget : TargetRules
 {
-	public EchoOfMeEditorTarget( TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
-		ExtraModuleNames.Add("EchoOfMe");
-	}
+    public EchoOfMeEditorTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Editor;
+
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+
+        ExtraModuleNames.Add("EchoOfMe");
+    }
 }
