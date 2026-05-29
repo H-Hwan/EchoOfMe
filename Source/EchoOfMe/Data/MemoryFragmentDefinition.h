b572cb0f130e0105cd4e01c3e3e354e6222e63c7
i69541620-glitch <wjdghks2339@gmail.com>
2026-05-29 18:34:32 +0900

    [MemoryFragmentActor]

---
+++ Source/EchoOfMe/Data/MemoryFragmentDefinition.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/InventoryItemDefinition.h"
+#include "MemoryFragmentDefinition.generated.h"
+
+
+UCLASS(BlueprintType)
+class ECHOOFME_API UMemoryFragmentDefinition : public UInventoryItemDefinition {
+	GENERATED_BODY()
+
+public:
+	// 회수 시 재생할 음성
+	UPROPERTY(EditDefaultsOnly, Category = "Memory")
+	TObjectPtr<USoundBase> FlashbackSound;
+
+	// 자막 텍스트
+	UPROPERTY(EditDefaultsOnly, Category = "Memory", meta = (MultiLine = true))
+	FText FlashbackText;
+
+	// 해당 조각으로 켜지는 플래그 키
+	UPROPERTY(EditDefaultsOnly, Category = "Memory")
+	FName FlagKey;
+};
