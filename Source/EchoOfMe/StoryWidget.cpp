2a804d653a62b92535a000e23581da121c04bab2
ekfsla246-hub <ekfsla246@gmail.com>
2026-06-09 16:31:18 +0900

    ui c++변환

---
+++ Source/EchoOfMe/StoryWidget.cpp
@@ -0,0 +1,8 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StoryWidget.h"
+
+void UStoryWidget::UpdateUI(const FText& InText, UTexture2D* InImage)
+{
+}
---
+++ Source/EchoOfMe/StoryWidget.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h" // 텍스트 위젯을 위해 필수
+#include "Components/Image.h"     //
+#include "StoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECHOOFME_API UStoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
