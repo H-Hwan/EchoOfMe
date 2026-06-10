4dcedf7d9c168e5c441868351156e4579060c4e7
SingungAA <4575kimkms@gmail.com>
2026-06-10 13:43:44 +0900

    6565998858

---
+++ Source/EchoOfMe/enemysounddetectionsensor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "enemysounddetectionsensor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECHOOFME_API Uenemysounddetectionsensor : public UAIPerceptionComponent
+{
+	GENERATED_BODY()
+	
+};
