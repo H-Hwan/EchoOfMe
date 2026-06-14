3c654871d0576d8f51c1f630f8fc99bb0706e990
Trk44344 <4575kimkms@gmail.com>
2026-06-14 18:31:55 +0900

    x

--- Source/EchoOfMe/Enemy/EchoEnemyAIController.cpp
+++ Source/EchoOfMe/Enemy/EchoEnemyAIController.cpp
@@ -3,16 +3,69 @@
 
 #include "EchoEnemyAIController.h"
 #include "Enemy/EchoEnemy.h"
-#include "NavigationSystem.h"
+
+#include "Kismet/GameplayStatics.h"
 
 
-void AEchoEnemyAIController::BeginPlay()
+void AEchoEnemyAIController::OnPossess(APawn* InPawn)
 {
-	Super::BeginPlay();
+	Super::OnPossess(InPawn);
 
-	OwnerPawn = Cast<AEchoEnemy>(GetOwner());
+	OwnerPawn = Cast<AEchoEnemy>(InPawn);
 
+	// 센서 탐색: 컨트롤러에 붙었으면 this, 폰에 붙었으면 InPawn에서
+	CachedSensor = FindComponentByClass<UResonanceSensorComponent>();
+	if (!CachedSensor && InPawn)
+	{
+		CachedSensor = InPawn->FindComponentByClass<UResonanceSensorComponent>();
+	}
 
-
+	if (CachedSensor)
+	{
+		CachedSensor->OnSenseLevelChanged.AddDynamic(this, &AEchoEnemyAIController::HandleSenseLevelChanged);
+		UE_LOG(LogTemp, Warning, TEXT("[EnemyAI] 센서 연결 완료"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("[EnemyAI] ResonanceSensorComponent 못 찾음 - 폰/컨트롤러 어디에 붙었는지 확인"));
+	}
 }
 
+
+void AEchoEnemyAIController::OnUnPossess()
+{
+	if (CachedSensor)
+	{
+		CachedSensor->OnSenseLevelChanged.RemoveDynamic(this, &AEchoEnemyAIController::HandleSenseLevelChanged);
+	}
+
+	Super::OnUnPossess();
+}
+
+
+void AEchoEnemyAIController::HandleSenseLevelChanged(ESenseChannel Channel, ESenseLevel Level)
+{
+	if (!CachedSensor) return;
+
+	if (Level == ESenseLevel::Alert)   // 추적
+	{
+		FVector Target;
+		if (Channel == ESenseChannel::Sound)
+		{
+			Target = CachedSensor->GetLastNoiseLocation();
+			UE_LOG(LogTemp, Warning, TEXT("[EnemyAI] 소리 추적 -> %s"), *Target.ToString());
+		}
+		else
+		{
+			APawn* Player = UGameplayStatics::GetPlayerPawn(this, 0);
+			if (!Player) return;
+			Target = Player->GetActorLocation();
+			UE_LOG(LogTemp, Warning, TEXT("[EnemyAI] 공명 추적 -> %s"), *Target.ToString());
+		}
+		MoveToLocation(Target, 100.f);
+	}
+	else if (Level == ESenseLevel::Calm)
+	{
+		StopMovement();
+	}
+}
--- Source/EchoOfMe/Enemy/EchoEnemyAIController.h
+++ Source/EchoOfMe/Enemy/EchoEnemyAIController.h
@@ -4,9 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+
+#include "Enemy/ResonanceSensorComponent.h"
+
 #include "EchoEnemyAIController.generated.h"
 
 class AEchoEnemy;
+
 
 UCLASS()
 class ECHOOFME_API AEchoEnemyAIController : public AAIController
@@ -14,16 +18,17 @@ class ECHOOFME_API AEchoEnemyAIController : public AAIController
 	GENERATED_BODY()
 
 protected:
-
+	UPROPERTY()
 	AEchoEnemy* OwnerPawn;
 
 
 public:
+	// 빙의/해제 시점에 센서 구독·해제
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void OnUnPossess() override;
 
-	virtual void BeginPlay() override;
-
-
-
-
-	
+	UPROPERTY()
+	TObjectPtr<UResonanceSensorComponent> CachedSensor;
+	UFUNCTION()
+	void HandleSenseLevelChanged(ESenseChannel Channel, ESenseLevel Level);
 };
